2a8b982e9873f109e6ff84440c60a14fcc801769
GeJintian <jintian.ge@qq.com>
2023-01-13 17:05:58 +0800

    Implement dLieAlg, dJ, dJi, dbeta.

--- fine_opt.py
+++ fine_opt.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import dot, transpose
 import cv2
 from utils.helpers import Cam2World, World2Cam, Doppler_velocity, Pos2Vel, Pos_transform
 
@@ -34,50 +35,73 @@ class optical_field():
         vz = (d1*(self.cu-u1)-d0*(self.cu-u))/(self.dt*self.f)
 
         return np.array([[vx],[vy],[vz],[0]])
+    
+    #We might need bilinear interpolation and central difference for optical field
 
-def Ji(cVc, cVr):
+def Ji(cVc, T, Vr):
     """return: compute Ji, which is the cost function for one single point"""
-    alpha = np.dot(np.transpose(cVc),cVr) # should be scalar
-    beta = np.dot(np.transpose(cVr),cVr) # should be scalar
+    cVr = dot(T,Vr)
+    alpha = dot(transpose(cVc),cVr) # should be scalar
+    beta = dot(transpose(cVr),cVr) # should be scalar
     return (alpha-beta)*cVr
 
-def J(cVcs, cVrs):
+def J(cVcs, T, Vrs):
     """return: the final cost function J = sum(Ji^T*Ji)"""
     loss = 0
     for i in range(cVcs):
-        loss += Ji(cVcs[i],cVrs[i])
+        ji = Ji(cVcs[i], T, Vrs[i])
+        loss += dot(transpose(ji),ji)
     return loss
 
-def dJ():
-    """differentiate of J"""
-    
-    return
+def dJi(cVc, T, Vr):
+    """differentiate of Ji"""
+    cVr = dot(T,Vr)
+    alpha = dot(transpose(cVc),cVr) # should be scalar
+    beta = dot(transpose(cVr),cVr) # should be scalar
+    gamma = alpha-beta
+    result = gamma * dLieAlg(T,Vr) + dot(cVr,dgamma(T, Vr))#TODO: pass in arguments for dgamma
+    return result
 
 def dLieAlg(T,P):
     """The input P could be a position or a velocity"""
+    p = P[:3] # inhomogeneous format
+    R = T[:3,:3]
+    t = T[:3,3]
+    t = transpose(np.array([t]))
+    Liealg = dot(R,p) + t
+    p1,p2,p3 = transpose(Liealg)[0]
+    anti_sym_mat = -np.array([[0,-p3,p2],[p3,0,-p1],[-p2,p1,0]])
+    result = np.hstack((np.diag([1,1,1]),anti_sym_mat))
+    result = np.vstack((result,np.zeros(6)))
+    return result
 
-    return
-
-def dalpha(cVr,cVc):
+def dalpha(T, Vr, cVc):#TODO: add more arguments for dopt
     """Derivative of alpha"""
-    
-    return
+    cVr = dot(T,Vr)
+    result = dot(transpose(cVr),dcVc()) + dot(transpose(cVc),dLieAlg(T,Vr))#TODO: finish dcVc
 
-def dbeta():
+    return result
+
+def dbeta(T,Vr):
     """Derivative of beta"""
-    
-    return
+    cVrT = transpose(dot(T,Vr))
+    dliealg = dLieAlg(T, Vr)
+    result = 2*dot(cVrT,dliealg)
+    return result
 
-def dgamma():
-    """Derivative of alpha+beta"""
-
-    return
+def dgamma(T, Vr, cVc):
+    """Derivative of alpha-beta"""
+    result = dalpha(T,Vr, cVc) - dbeta(T,Vr) #TODO:add more arguments
+    return result
 
 def dopt():
-    """Derivative of cVc. Using central difference"""
+    """Derivative of optical flow velocity. Using central difference"""
 
     return
 
+def dcVc():
+    """Derivative of cVc. dcVc = dopt*dLieAlg(T,Pr) """
+    return
 def fine_optimize():
     """fine opt is implemented in a gradient descent manner"""
 
